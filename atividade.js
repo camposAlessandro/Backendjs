b1ecc41552b1af04763f339009de2e16074875c7
camposAlessandro <camposalessandro1234@gmail.com>
2025-10-02 08:57:13 -0300

    Atividade em .js

---
+++ atividade.js
@@ -0,0 +1,16 @@
+const nome = 'Alessandro';
+const sobrenome = 'Campos';
+const idade = 19;
+const peso = 70;
+const altura = 1.78;
+
+const IMC = peso / (altura * altura);
+
+console.log(nome + ' ' + sobrenome + ' tem ' + idade + ' anos, pesa ' + peso + ' kg. Tem ' + altura + ' m altura e seu IMC e de ' + IMC);
+
+
+
+
+
+
+
